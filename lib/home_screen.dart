578e2d622e8c5c46548ef37ff9e1517b6204bd4f
Anuj Kushwah <anujkushwah7300@gmail.com>
2026-06-27 01:14:49 +0530

    api data integrated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'model.dart';
 
+class Weather {
+  final String city;
+  final int temperature;
+  final int windSpeed;
+  final int humidity;
+  // final int aqi;
+
+  Weather({required this.city, required this.temperature, required this.windSpeed, required this.humidity});
+
+  Weather.fromJson(Map<String, dynamic> json) :
+        city = json['location']['name'],
+        temperature = json['current']['temp_c'].toInt(),
+        windSpeed = json['current']['wind_kph'].toInt(),
+        humidity = json['current']['humidity'].toInt();
+
+}
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,18 +27,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var city = "Agra";
-  var temperature = "35 ℃";
-  var windSpeed  = "12 km/h";
-  var humidity = "30%";
-  var aqi = 100;
-
-  late Future<WeatherUpdates> weatherUpdates;
+  late Future<Weather> weatherUpdates;
 
   @override
   void initState() {
     super.initState();
-    fetchWeather();
+    weatherUpdates = fetchWeather("Agra");
+
   }
 
   @override
@@ -32,104 +44,116 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Mausam',style: TextStyle(color: Colors.white, fontSize: 36,fontWeight: FontWeight.bold),),
         backgroundColor: Colors.blue.shade800,
       ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        padding: EdgeInsets.all(8),
-        children: <Widget>[
-          Container(
-            height: 200,
-            width: MediaQuery.of(context).size.width,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              gradient: LinearGradient(colors: [Colors.blue.shade50,Colors.blue.shade400],begin: Alignment.bottomLeft,end: Alignment.topRight),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  temperature,
-                  style: TextStyle(
-                    fontSize: 64,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue.shade900,
-                  ),
-                ),
-                Text(
-                  city,
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue.shade900,
-                  ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            height: 80,
-            margin: EdgeInsets.symmetric(vertical: 12),
-            width: MediaQuery.of(context).size.width,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              gradient: LinearGradient(colors: [Colors.blue.shade50,Colors.blue.shade400],begin: Alignment.bottomLeft,end: Alignment.topRight),
-            ),
-            child: Text(
-              "AQI : $aqi",
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children:[
-              Expanded(
-                child: Container(
-                  height: 150,
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    gradient: LinearGradient(colors: [Colors.blue.shade50,Colors.blue.shade400],begin: Alignment.bottomLeft,end: Alignment.topRight),
-                  ),
-                  child: Text(
-                    windSpeed,
-                    style: TextStyle(
-                      fontSize: 36,
-                      fontWeight: FontWeight.bold,
+      body: FutureBuilder<Weather>(
+          future: weatherUpdates, builder: (context, snapshot){
+            if(snapshot.hasData){
+              return ListView(
+                scrollDirection: Axis.vertical,
+                padding: EdgeInsets.all(8),
+                children: <Widget>[
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      gradient: LinearGradient(colors: [Colors.blue.shade50,Colors.blue.shade400],begin: Alignment.bottomLeft,end: Alignment.topRight),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${snapshot.data!.temperature}°C',
+                          style: TextStyle(
+                            fontSize: 64,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue.shade900,
+                          ),
+                        ),
+                        Text(
+                          snapshot.data!.city,
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue.shade900,
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                ),
-              ),
-              SizedBox(width: 10,),
-              Expanded(
-                child: Container(
-                  height: 150,
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    gradient: LinearGradient(colors: [Colors.blue.shade50,Colors.blue.shade400],begin: Alignment.bottomLeft,end: Alignment.topRight),
-                  ),
-                  child: Text(
-                    humidity,
-                    style: TextStyle(
-                      fontSize: 36,
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    height: 80,
+                    margin: EdgeInsets.symmetric(vertical: 12),
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      gradient: LinearGradient(colors: [Colors.blue.shade50,Colors.blue.shade400],begin: Alignment.bottomLeft,end: Alignment.topRight),
+                    ),
+                    child: Text(
+                      "AQI : 100",
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children:[
+                      Expanded(
+                        child: Container(
+                          height: 150,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            gradient: LinearGradient(colors: [Colors.blue.shade50,Colors.blue.shade400],begin: Alignment.bottomLeft,end: Alignment.topRight),
+                          ),
+                          child: Text(
+                            '${snapshot.data!.windSpeed}km/h',
+                            style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Expanded(
+                        child: Container(
+                          height: 150,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            gradient: LinearGradient(colors: [Colors.blue.shade50,Colors.blue.shade400],begin: Alignment.bottomLeft,end: Alignment.topRight),
+                          ),
+                          child: Text(
+                            '${snapshot.data!.humidity}%',
+                            style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            }
+            else if(snapshot.hasError){
+              return Text('${snapshot.error}');
+            }
+            else{
+              return CircularProgressIndicator();
+            }
+          }
+      )
     );
   }
 }
--- lib/model.dart
+++ lib/model.dart
@@ -2,34 +2,18 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import 'home_screen.dart';
+
 String key = "eb6f3a2c86b849c3880174258262606";
-Future<WeatherUpdates> fetchWeather() async {
-  final response = await http.get(Uri.parse("https://api.weatherapi.com/v1/current.json?$key&q=Agra"),
+Future<Weather> fetchWeather(String city) async {
+  final response = await http.get(Uri.parse("https://api.weatherapi.com/v1/current.json?key=$key&q=Agra"),
     headers: {'Accept': 'application/json'},
   );
   if(response.statusCode == 200){
-    print(response.body);
-    return WeatherUpdates.fromJson(jsonDecode(response.body));
+    return Weather.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   }else{
     throw Exception('Failed to load weather data');
 
   }
 }
 
-class WeatherUpdates {
-  late String city;
-  late int temperature;
-  late int windSpeed;
-  late int humidity;
-  late int aqi;
-
-  WeatherUpdates({required this.city});
-
-  WeatherUpdates.fromJson(Map<String, dynamic> json){
-    city = json['location']['name'];
-    temperature = json['current']['temp_c'];
-    windSpeed = json['current']['wind_kph'];
-    humidity = json['current']['humidity'];
-    aqi = json['current']['air_quality']['pm2_5'];
-  }
-}
