fd5eb6edd933b5fae98e7545bb4fb3dc396762a1
Anuj Kushwah <anujkushwah7300@gmail.com>
2026-06-28 01:51:44 +0530

    Sliver app bar impemented with variable data

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -4,18 +4,26 @@ class Weather {
   final int windSpeed;
   final int humidity;
   final int feelsLike;
+  final int uv;
+  final String condition;
   // final int aqi;
 
-  Weather({required this.city, required this.temperature, required this.windSpeed, required this.humidity,required this.feelsLike});
+  Weather({
+    required this.city,
+    required this.temperature,
+    required this.windSpeed,
+    required this.humidity,
+    required this.feelsLike,
+    required this.uv,
+    required this.condition
+  });
 
-  Weather.fromJson(Map<String, dynamic> json) :
-        city = json['location']['name'],
-        temperature = json['current']['temp_c'].toInt(),
-        windSpeed = json['current']['wind_kph'].toInt(),
-        humidity = json['current']['humidity'].toInt(),
-        feelsLike = json['current']['feelslike_c'].toInt();
-
+  Weather.fromJson(Map<String, dynamic> json)
+    : city = json['location']['name'],
+      temperature = json['current']['temp_c'].toInt(),
+      windSpeed = json['current']['wind_kph'].toInt(),
+      humidity = json['current']['humidity'].toInt(),
+      feelsLike = json['current']['feelslike_c'].toInt(),
+      uv = json['current']['uv'].toInt(),
+      condition = json['current']['condition']['text'];
 }
-
-
-
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:mausam/Widgets/box_decoration.dart';
 import '../models/model.dart';
 import '../services/location_service.dart';
 import '../services/weather_service.dart';
@@ -13,16 +14,19 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late Future<Weather> weatherUpdates;
-  late Position position;
 
   Future<void> fetchLocation() async {
-    position = await determinePosition();
+    Position position = await determinePosition();
     setState(() {
-      weatherUpdates = fetchWeather(position.latitude.toString(), position.longitude.toString());
+      weatherUpdates = fetchWeather(
+        position.latitude.toString(),
+        position.longitude.toString(),
+      );
     });
   }
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     fetchLocation();
   }
@@ -30,40 +34,77 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffD8FDFF),
-      body: CustomScrollView(
-        scrollDirection: Axis.vertical,
-        slivers: <Widget>[
-          const SliverAppBar(
-            title: Text(
-              'Mausam',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            expandedHeight: 200,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(),
-            backgroundColor: Colors.blue,
-          ),
-          SliverToBoxAdapter(
-            child: FutureBuilder<Weather>(
-              future: weatherUpdates,
-              builder: (context, snapshot) {
-                if(snapshot.connectionState == ConnectionState.waiting){
-                  return Center(child: CircularProgressIndicator());
-                }
-                if (snapshot.hasError) {
-                  return Center(child: Text(snapshot.error.toString()));
-                }
-                if (!snapshot.hasData) {
-                  return const SizedBox();
-                }
+      backgroundColor: Colors.blue.shade100,
+      body: FutureBuilder<Weather>(
+        future: weatherUpdates,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+          if (!snapshot.hasData) {
+            return const SizedBox();
+          }
 
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
+          final weather = snapshot.data!;
+          return CustomScrollView(
+            scrollDirection: Axis.vertical,
+            slivers: <Widget>[
+              SliverAppBar(
+                title: Text(
+                  snapshot.data!.city,
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue.shade900,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.search, size: 36),
+                    color: Colors.blue.shade900,
+                    padding: EdgeInsets.only(right: 8),
+                  ),
+                ],
+                shadowColor: Colors.blue.shade100,
+                expandedHeight: 200,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${snapshot.data!.temperature}°C',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 56,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          snapshot.data!.condition,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                backgroundColor: Colors.blue.shade100,
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
                   child: Column(
                     children: <Widget>[
                       Container(
@@ -71,20 +112,13 @@ class _HomeScreenState extends State<HomeScreen> {
                         width: MediaQuery.of(context).size.width,
                         alignment: Alignment.centerLeft,
                         padding: EdgeInsets.all(16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          gradient: LinearGradient(
-                            colors: [Colors.blue.shade50, Colors.blue.shade400],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight,
-                          ),
-                        ),
+                        decoration: decoration,
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              '${snapshot.data!.temperature}°C',
+                              '${weather.temperature}°C',
                               style: TextStyle(
                                 fontSize: 64,
                                 fontWeight: FontWeight.bold,
@@ -100,7 +134,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                             ),
                             Text(
-                              'Feels like : ${snapshot.data!.feelsLike}°C',
+                              'Feels like : ${weather.feelsLike}°C',
                               style: TextStyle(
                                 fontSize: 24,
                                 fontWeight: FontWeight.bold,
@@ -110,28 +144,45 @@ class _HomeScreenState extends State<HomeScreen> {
                           ],
                         ),
                       ),
+                      SizedBox(height: 12),
                       Container(
-                        height: 80,
-                        margin: EdgeInsets.symmetric(vertical: 12),
+                        height: 200,
                         width: MediaQuery.of(context).size.width,
                         alignment: Alignment.centerLeft,
                         padding: EdgeInsets.all(16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          gradient: LinearGradient(
-                            colors: [Colors.blue.shade50, Colors.blue.shade400],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight,
-                          ),
-                        ),
-                        child: Text(
-                          "AQI : 100",
-                          style: TextStyle(
-                            fontSize: 36,
-                            fontWeight: FontWeight.bold,
-                          ),
+                        decoration: decoration,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${weather.temperature}°C',
+                              style: TextStyle(
+                                fontSize: 64,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue.shade900,
+                              ),
+                            ),
+                            Text(
+                              weather.city,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue.shade900,
+                              ),
+                            ),
+                            Text(
+                              'Feels like : ${weather.feelsLike}°C',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue.shade900,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
+                      SizedBox(height: 12),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
@@ -140,19 +191,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               height: 150,
                               alignment: Alignment.center,
                               padding: EdgeInsets.all(16),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                gradient: LinearGradient(
-                                  colors: [
-                                    Colors.blue.shade50,
-                                    Colors.blue.shade400,
-                                  ],
-                                  begin: Alignment.bottomLeft,
-                                  end: Alignment.topRight,
-                                ),
-                              ),
+                              decoration: decoration,
                               child: Text(
-                                '${snapshot.data!.windSpeed}km/h',
+                                '${weather.windSpeed}km/h',
                                 style: TextStyle(
                                   fontSize: 36,
                                   fontWeight: FontWeight.bold,
@@ -166,19 +207,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               height: 150,
                               alignment: Alignment.center,
                               padding: EdgeInsets.all(16),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                gradient: LinearGradient(
-                                  colors: [
-                                    Colors.blue.shade50,
-                                    Colors.blue.shade400,
-                                  ],
-                                  begin: Alignment.bottomLeft,
-                                  end: Alignment.topRight,
-                                ),
-                              ),
+                              decoration: decoration,
                               child: Text(
-                                '${snapshot.data!.humidity}%',
+                                '${weather.humidity}%',
                                 style: TextStyle(
                                   fontSize: 36,
                                   fontWeight: FontWeight.bold,
@@ -188,73 +219,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ],
                       ),
-                      SizedBox(height: 12,),
-                      Container(
-                        height: 200,
-                        width: MediaQuery.of(context).size.width,
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.all(16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          gradient: LinearGradient(
-                            colors: [Colors.blue.shade50, Colors.blue.shade400],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight,
-                          ),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${snapshot.data!.temperature}°C',
-                              style: TextStyle(
-                                fontSize: 64,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue.shade900,
-                              ),
-                            ),
-                            Text(
-                              snapshot.data!.city,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue.shade900,
-                              ),
-                            ),
-                            Text(
-                              'Feels like : ${snapshot.data!.feelsLike}°C',
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue.shade900,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        height: 80,
-                        margin: EdgeInsets.symmetric(vertical: 12),
-                        width: MediaQuery.of(context).size.width,
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.all(16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          gradient: LinearGradient(
-                            colors: [Colors.blue.shade50, Colors.blue.shade400],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight,
-                          ),
-                        ),
-                        child: Text(
-                          "AQI : 100",
-                          style: TextStyle(
-                            fontSize: 36,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
+                      SizedBox(height: 12),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
@@ -263,19 +228,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               height: 150,
                               alignment: Alignment.center,
                               padding: EdgeInsets.all(16),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                gradient: LinearGradient(
-                                  colors: [
-                                    Colors.blue.shade50,
-                                    Colors.blue.shade400,
-                                  ],
-                                  begin: Alignment.bottomLeft,
-                                  end: Alignment.topRight,
-                                ),
-                              ),
+                              decoration: decoration,
                               child: Text(
-                                '${snapshot.data!.windSpeed}km/h',
+                                '${weather.uv}',
                                 style: TextStyle(
                                   fontSize: 36,
                                   fontWeight: FontWeight.bold,
@@ -289,19 +244,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               height: 150,
                               alignment: Alignment.center,
                               padding: EdgeInsets.all(16),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                gradient: LinearGradient(
-                                  colors: [
-                                    Colors.blue.shade50,
-                                    Colors.blue.shade400,
-                                  ],
-                                  begin: Alignment.bottomLeft,
-                                  end: Alignment.topRight,
-                                ),
-                              ),
+                              decoration: decoration,
                               child: Text(
-                                '${snapshot.data!.humidity}%',
+                                '${weather.humidity}%',
                                 style: TextStyle(
                                   fontSize: 36,
                                   fontWeight: FontWeight.bold,
@@ -313,11 +258,11 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ],
                   ),
-                );
-              },
-            ),
-          ),
-        ],
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
